6ea5f4227bfc015b50d17f292d37647b97a5ed39
kennedy2244 <kennedy_2244@yu.ac.kr>
2026-04-19 19:20:29 +0900

    fix: resolve check error

--- R/generics.R
+++ R/generics.R
@@ -1,5 +1,6 @@
 #' @import methods
-#' @importFrom stats simulate dnorm dpois optim qnorm quantile median density lag
+#' @importFrom stats dnorm dpois optim qnorm quantile median density lag
+#' @importFrom stats simulate
 #' @importFrom numDeriv hessian
 #' @importFrom ggplot2 ggplot aes geom_line geom_ribbon geom_histogram
 #'   geom_density geom_col geom_hline labs theme_minimal after_stat
